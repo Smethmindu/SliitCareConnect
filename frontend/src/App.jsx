8cb6af463ce65b46d733605171ff9d3919f70556
Smethmindu <smethmindu@gmail.com>
2026-04-08 19:56:51 +0530

    about us page deletion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
-import { AboutUs } from "./pages/AboutUs";
 import { ContactPage } from "./pages/ContactPage";
 import { MessagesPage } from "./pages/MessagesPage";
 import { UsersPage } from "./pages/UsersPage";
@@ -20,7 +19,6 @@ export default function App() {
         {/* Public / Auth Routes */}
         <Route element={<AMainLayout />}>
           <Route path="/" element={<LoginPage />} />
-          <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
